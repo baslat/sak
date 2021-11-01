d2cf02c3490c5ad40729d45a7250ca2645fe3d78
Bas Latcham <abasi.latcham@gmail.com>
2021-11-02 08:17:52 +1100

    Adds on_path()

---
+++ R/on_path.r
@@ -0,0 +1,42 @@
+#' Check if something is on the system path
+#'
+#' Check if something is on the system path. It does this by using the
+#' `where.exe` utility.
+#'
+#' @param what (character) a string, typically to file ,
+#'   where backslashes are replaced with double underscores!
+#' @param verbose (logical; default = `TRUE`) message the path location?
+#'
+#' @return logical, \code{TRUE} if a match is found
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Is Rscript
+#' on_path(what = "Rscript")
+#' }
+on_path <- function(what, verbose = TRUE) {
+    # only runs on windows
+    if (!is.windows()) {
+        message("This only runs on Windows")
+        invisible(FALSE)
+    }
+
+    # where.exe is in C:\Windows\System32\where.exe, so should be usable
+    res <- suppressWarnings(
+        system2("where.exe",
+            args = what,
+            stdout = TRUE,
+            stderr = NULL
+        )
+    ) %>%
+        strsplit(";")
+
+    if (verbose) {
+        res %>%
+            paste(collapse = "\n") %>%
+            message()
+    }
+
+    !rlang::is_empty(res)
+}
