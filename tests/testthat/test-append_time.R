3c581435a0d83d9ae0414a67aa9bcb5a799cdeb2
Bas Latcham <abasi.latcham@gmail.com>
2021-11-02 08:06:16 +1100

    Styles tests

--- tests/testthat/test-append_time.R
+++ tests/testthat/test-append_time.R
@@ -1,10 +1,12 @@
 test_that("append_time works", {
-
   filename <- basename(
     append_time("test.csv",
-                time = as.POSIXct(strptime("2010-10-31 01:30:00", "%Y-%m-%d %H:%M:%S"))))
+      time = as.POSIXct(strptime("2010-10-31 01:30:00", "%Y-%m-%d %H:%M:%S"))
+    )
+  )
 
-  expect_equal(filename,
-               "test_2010_10_31_0130.csv")
-
+  expect_equal(
+    filename,
+    "test_2010_10_31_0130.csv"
+  )
 })
