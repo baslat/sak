3c8b27724459fbc940ab9d082945e4c022553408
Bas Latcham <bas@latcham.au>
2022-07-11 20:56:46 +1000

    fix: :bug: Fix `on_path()` to alert for non-Windows use

--- R/on_path.r
+++ R/on_path.r
@@ -1,4 +1,4 @@
-#' Check if something is on the system path
+#' Check if something is on the system path (Windows only)
 #'
 #' Check if something is on the system path. It does this by using the
 #' `where.exe` utility.
@@ -15,7 +15,12 @@
 #' on_path(what = "Rscript")
 #' }
 on_path <- function(what, verbose = TRUE) {
-  function_needs("fs")
+  # only runs on windows
+  if (.Platform[["OS.type"]] != "windows") {
+    message("This only runs on Windows")
+    invisible(FALSE)
+  }
+
   # where.exe is in C:\Windows\System32\where.exe, so should be usable
   res <- run_well("where.exe",
     args = what,
